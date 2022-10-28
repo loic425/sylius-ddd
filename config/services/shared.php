52f7ceda8201b3c58f307173ba38a137a30ac76c
Loïc Frémont <lc.fremont@gmail.com>
2022-10-28 15:02:00 +0200

    Add menu

--- config/services/shared.php
+++ config/services/shared.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Shared\Infrastructure\Sylius\Menu\AdminMenuBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -15,4 +16,9 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services->load('App\\Shared\\', __DIR__.'/../../src/Shared')
         ->exclude([__DIR__.'/../../src/Shared/Infrastructure/Kernel.php']);
+
+    $services
+        ->set(AdminMenuBuilder::class)
+        ->decorate('sylius.ux.menu.admin')
+    ;
 };
---
+++ src/Shared/Infrastructure/Sylius/Menu/AdminMenuBuilder.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Monofony demo project.
+ *
+ * (c) Monofony
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Shared\Infrastructure\Sylius\Menu;
+
+use Knp\Menu\ItemInterface;
+use Sylius\Ux\Core\Menu\MenuBuilderInterface;
+
+final class AdminMenuBuilder implements MenuBuilderInterface
+{
+    public function __construct(private MenuBuilderInterface $menuBuilder)
+    {
+    }
+
+    public function createMenu(array $options): ItemInterface
+    {
+        $menu = $this->menuBuilder->createMenu($options);
+
+        $this->addLibrarySubMenu($menu);
+
+        return $menu;
+    }
+
+    private function addLibrarySubMenu(ItemInterface $menu): void
+    {
+        $library = $menu
+            ->addChild('library')
+            ->setLabel('app.ui.library')
+        ;
+
+        $library->addChild('backend_pet', ['route' => 'app_admin_book_index'])
+            ->setLabel('app.ui.books')
+            ->setLabelAttribute('icon', 'book');
+    }
+}
