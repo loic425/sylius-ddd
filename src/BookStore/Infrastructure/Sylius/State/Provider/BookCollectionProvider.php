20544fa083b82d80cd9c3832f9e03981e5d3f239
Loïc Frémont <lc.fremont@gmail.com>
2023-03-08 09:11:35 +0100

    Fix Book collection provider

--- src/BookStore/Infrastructure/Sylius/State/Provider/BookCollectionProvider.php
+++ src/BookStore/Infrastructure/Sylius/State/Provider/BookCollectionProvider.php
@@ -4,32 +4,20 @@ declare(strict_types=1);
 
 namespace App\BookStore\Infrastructure\Sylius\State\Provider;
 
-use App\BookStore\Infrastructure\Sylius\Repository\BookRepository;
-use Sylius\Bundle\ResourceBundle\Controller\ResourcesCollectionProviderInterface;
 use Sylius\Component\Resource\Context\Context;
-use Sylius\Component\Resource\Context\Option\RequestConfigurationOption;
+use Sylius\Component\Resource\Grid\State\RequestGridProvider;
 use Sylius\Component\Resource\Metadata\Operation;
 use Sylius\Component\Resource\State\ProviderInterface;
-use Webmozart\Assert\Assert;
 
 final class BookCollectionProvider implements ProviderInterface
 {
     public function __construct(
-        private ResourcesCollectionProviderInterface $resourcesCollectionProvider,
-        private BookRepository $bookRepository,
+        private RequestGridProvider $requestGridProvider,
     ) {
     }
 
     public function provide(Operation $operation, Context $context): object|iterable
     {
-        $requestionConfigurationOption = $context->get(RequestConfigurationOption::class);
-        Assert::notNull($requestionConfigurationOption);
-
-        $configuration = $requestionConfigurationOption->configuration();
-
-        return $this->resourcesCollectionProvider->get(
-            $configuration,
-            $this->bookRepository,
-        );
+        return $this->requestGridProvider->provide($operation, $context);
     }
 }
